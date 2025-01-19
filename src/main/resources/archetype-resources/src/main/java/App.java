a9f2f30c649f0ec8e6f9c2f4b6f6e60d44354470
slavk <slavka.cherenkov@gmail.com>
2025-01-19 21:15:50 +0300

    add HelloWorld

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ru.cherenkov.preproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Создание и настройка Git" );
+    }
+}
